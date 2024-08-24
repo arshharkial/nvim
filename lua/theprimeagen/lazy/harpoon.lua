c71e2dee2e87bd323c50b0350b51b89f91a79520
arshdeepharkial <arsh@clouddefense.ai>
2024-08-24 10:40:54 +0530

    Update Keybindings

--- lua/theprimeagen/lazy/harpoon.lua
+++ lua/theprimeagen/lazy/harpoon.lua
@@ -10,13 +10,15 @@ return {
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        vim.keymap.set("n", "<M-h>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<M-j>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<M-k>", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<M-l>", function() harpoon:list():select(4) end)
-        vim.keymap.set("n", "<leader><M-h>", function() harpoon:list():replace_at(1) end)
-        vim.keymap.set("n", "<leader><M-j>", function() harpoon:list():replace_at(2) end)
-        vim.keymap.set("n", "<leader><M-k>", function() harpoon:list():replace_at(3) end)
-        vim.keymap.set("n", "<leader><M-l>", function() harpoon:list():replace_at(4) end)
+        vim.keymap.set("n", "<M-1>", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<M-2>", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<M-3>", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<M-4>", function() harpoon:list():select(4) end)
+        vim.keymap.set("n", "<M-5>", function() harpoon:list():select(5) end)
+        vim.keymap.set("n", "<leader><M-1>", function() harpoon:list():replace_at(1) end)
+        vim.keymap.set("n", "<leader><M-2>", function() harpoon:list():replace_at(2) end)
+        vim.keymap.set("n", "<leader><M-3>", function() harpoon:list():replace_at(3) end)
+        vim.keymap.set("n", "<leader><M-4>", function() harpoon:list():replace_at(4) end)
+        vim.keymap.set("n", "<leader><M-5>", function() harpoon:list():replace_at(5) end)
     end
 }
