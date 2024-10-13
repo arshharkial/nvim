88838eaeb080f3d5a54cdef46d34f85f64257219
arshdeepharkial <arsh@clouddefense.ai>
2024-10-13 11:30:20 +0530

    Update session; add remote connection

--- lua/theprimeagen/lazy/sessions.lua
+++ lua/theprimeagen/lazy/sessions.lua
@@ -1,21 +1,20 @@
--- return {
---     'rmagatti/auto-session',
---     lazy = false,
---     dependencies = {
---         'nvim-telescope/telescope.nvim', -- Only needed if you want to use session lens
---     },
---     config = function()
---         require("auto-session").setup({
---             log_level = "error",
---             auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
---             session_lens = {
---                 buftypes_to_ignore = {},
---                 load_on_setup = true,
---                 theme_conf = { border = true },
---                 previewer = false,
---             },
---             vim.keymap.set("n", "<leader>ls", require("auto-session.session-lens").search_session, {})
---         })
---     end
--- }
-return {}
+return {
+    'rmagatti/auto-session',
+    lazy = false,
+    dependencies = {
+        'nvim-telescope/telescope.nvim', -- Only needed if you want to use session lens
+    },
+    config = function()
+        require("auto-session").setup({
+            log_level = "error",
+            auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+            session_lens = {
+                buftypes_to_ignore = {},
+                load_on_setup = true,
+                theme_conf = { border = true },
+                previewer = false,
+            },
+            vim.keymap.set("n", "<leader>ls", require("auto-session.session-lens").search_session, {})
+        })
+    end
+}
