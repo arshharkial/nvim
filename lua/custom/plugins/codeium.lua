c093d9ff0e11baa16cd5b3f4adbe0de1ec264397
Arshdeep Singh <arshdeep.singh@dentsu.com>
2025-04-20 12:44:47 +0530

    fix: remove codeium

--- lua/custom/plugins/codeium.lua
+++ lua/custom/plugins/codeium.lua
@@ -1,12 +1,14 @@
-return {
-    "Exafunction/codeium.nvim",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "hrsh7th/nvim-cmp",
-    },
-    config = function()
-        require("codeium").setup({
-            enable_chat = true,
-        })
-    end
-}
+-- return {
+--     "Exafunction/codeium.nvim",
+--     dependencies = {
+--         "nvim-lua/plenary.nvim",
+--         "hrsh7th/nvim-cmp",
+--     },
+--     config = function()
+--         require("codeium").setup({
+--             enable_chat = true,
+--         })
+--     end
+-- }
+
+return {}
