dc32fa0c09a17a91d4aeb338438b10ba4230d3ce
Arsh Deep Singh <arshharkial@Arshs-MacBook-Pro.local>
2025-04-26 16:31:17 +0530

    chore: Update Codeium/windsurf; Add sessions; Update keymaps

--- init.lua
+++ init.lua
@@ -235,21 +235,21 @@ vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
 vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz')
 vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
 
-vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+-- vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true })
 
 vim.keymap.set('n', '<leader>ee', 'oif err != nil {<CR>}<Esc>Oreturn err<Esc>')
 
 vim.keymap.set('n', '<leader>mr', '<cmd>CellularAutomaton make_it_rain<CR>')
 
-vim.keymap.set('n', '<leader><leader>', function()
-  vim.cmd 'so'
-end)
+-- vim.keymap.set('n', '<leader><leader>', function()
+--   vim.cmd 'so'
+-- end)
 
 -- Create Empty Buffer
 vim.keymap.set('n', '<leader>nb', ':enew<CR>', {})
 
-vim.keymap.set('n', '<leader>ct', ':CloakToggle<CR>', { silent = true })
+vim.keymap.set('n', '<leader>ct', ':CloakToggle<CR>', { silent = true, desc = '[C]loak [T]oggle' })
 
 -- Toggle Wrap
 vim.keymap.set('n', '<C-s>', ':set wrap!<CR>', { silent = true })
@@ -265,8 +265,8 @@ vim.keymap.set('n', '<leader>[', function()
   require('todo-comments').jump_prev()
 end, { desc = 'Previous todo comment' })
 
-vim.keymap.set('n', '<leader>st', ':TodoTelescope<CR>', { silent = true })
-vim.keymap.set('n', '<leader>db', ':DBUI<CR>', { silent = true })
+vim.keymap.set('n', '<leader>st', ':TodoTelescope<CR>', { silent = true, desc = '[S]earch [T]oDo Telescope' })
+vim.keymap.set('n', '<leader>db', ':DBUI<CR>', { silent = true, desc = '[DB] UI' })
 -- vim.keymap.set("n", "<M-.>", "<C-w>+<CR>", { noremap = false, silent = true })
 -- vim.keymap.set("n", "<M-,>", "<C-w>-<CR>", { noremap = false, silent = true })
 -- vim.keymap.set("n", "<C->>", "<C-w>><CR>", { noremap = false, silent = true })
---
+++ lua/custom/plugins/sessions.lua
@@ -0,0 +1,20 @@
+return {
+  'rmagatti/auto-session',
+  lazy = false,
+  dependencies = {
+    'nvim-telescope/telescope.nvim', -- Only needed if you want to use session lens
+  },
+  config = function()
+    require('auto-session').setup {
+      log_level = 'error',
+      -- auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+      session_lens = {
+        buftypes_to_ignore = {},
+        load_on_setup = true,
+        theme_conf = { border = true },
+        previewer = false,
+      },
+      vim.keymap.set('n', '<leader>sss', require('auto-session.session-lens').search_session, { desc = '[S]earch [S]ession[S]' }),
+    }
+  end,
+}
