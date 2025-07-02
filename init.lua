3375b80d535796e7cb8c1f438a6c5ab3847cfd80
Arsh Deep Singh <arshharkial@Arshs-MacBook-Pro.local>
2025-07-02 19:31:40 +0530

    chore: Remove Neogit and git-worktree

--- init.lua
+++ init.lua
@@ -573,7 +573,6 @@ require('lazy').setup({
       -- Enable Telescope extensions if they are installed
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
-      pcall(require('telescope').load_extension, 'git_worktree')
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
